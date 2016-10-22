7c1c3bec9a7ae3af282370356cbb8937970d83e1
Andrew Jarombek <ajarombek95@gmail.com>
2016-10-22 17:35:04 -0400

    Set up ActionBar xml and listener

--- app/src/main/java/com/example/andy/saints_xctf_android/MainActivity.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.andy.saints_xctf_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    /**
+     * Android Activity method for taking an action when an item is clicked in the
+     * app bar
+     * @param item menu item selected
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_log:
+                return true;
+            case R.id.action_home:
+                return true;
+            case R.id.action_profile:
+                return true;
+            case R.id.action_group:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                // The user's action was not recognized, invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
