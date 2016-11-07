55a5d3ac728a02d03ef1df65fe2d358a3b3e916b
Andrew Jarombek <ajarombek95@gmail.com>
2016-11-07 15:50:28 -0500

    V.0.0.1 - Set Up MainFragment

--- app/src/main/java/com/example/andy/saints_xctf_android/MainActivity.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/MainActivity.java
@@ -3,41 +3,20 @@ package com.example.andy.saints_xctf_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity: ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "Inside onCreate.");
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    /**
-     * Android Activity method for taking an action when an item is clicked in the
-     * app bar
-     * @param item menu item selected
-     * @return
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_log:
-                return true;
-            case R.id.action_home:
-                return true;
-            case R.id.action_profile:
-                return true;
-            case R.id.action_group:
-                return true;
-            case R.id.action_settings:
-                return true;
-            default:
-                // The user's action was not recognized, invoke the superclass to handle it.
-                return super.onOptionsItemSelected(item);
-
-        }
     }
 }
---
+++ app/src/main/java/com/example/andy/saints_xctf_android/MainFragment.java
@@ -0,0 +1,81 @@
+package com.example.andy.saints_xctf_android;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * @author Andrew Jarombek
+ * @since 11/7/2016 -
+ */
+public class MainFragment extends Fragment {
+
+    private View v;
+    private static final String TAG = "MainFragment: ";
+
+    /**
+     * Android onCreateView method
+     * @param inflater --
+     * @param container --
+     * @param savedInstanceState --
+     * @return fragment view
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.main, container, false);
+        Log.d(TAG, "Inside onCreateView.");
+
+        setHasOptionsMenu(true);
+        this.v = view;
+
+        return v;
+    }
+
+    /**
+     * Android onCreate method
+     * @param savedInstanceState --
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "Inside onCreate.");
+        // Save on rotation
+        setRetainInstance(true);
+    }
+
+    /**
+     * Android Activity method for taking an action when an item is clicked in the
+     * app bar
+     * @param item menu item selected
+     * @return --
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_log:
+                LogDialogFragment logDialog = new LogDialogFragment();
+                logDialog.show(getFragmentManager(), "log dialog");
+                return true;
+            case R.id.action_home:
+                return true;
+            case R.id.action_profile:
+                return true;
+            case R.id.action_group:
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                // The user's action was not recognized, invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
