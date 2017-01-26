5a7e29f311079887a2d8282bf2486a1bba8f0053
Andrew Jarombek <ajarombek95@gmail.com>
2017-01-26 15:25:39 -0500

    V.0.0.32 - Log Form Validation Good

--- app/src/main/java/com/example/andy/saints_xctf_android/LogDialogFragment.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/LogDialogFragment.java
@@ -49,7 +49,6 @@ public class LogDialogFragment extends DialogFragment {
     private Spinner log_type;
     private EditText log_distance;
     private Spinner log_metric;
-    private EditText log_time_hours;
     private EditText log_time_minutes;
     private EditText log_time_seconds;
     private SeekBar log_feel;
@@ -57,6 +56,8 @@ public class LogDialogFragment extends DialogFragment {
     private TextView log_error_message;
     private Calendar calendar;
     private AlertDialog d;
+    private int feel;
+    private String name,location,type,distance,metric,minutes,seconds,description;
 
     /**
      * Create and Run an AlertDialog for Log Submitting
@@ -168,76 +169,19 @@ public class LogDialogFragment extends DialogFragment {
                 @Override
                 public void onClick(View v) {
 
-                    String name = log_run_name.getText().toString();
-                    String location = log_location.getText().toString();
-                    String type = log_type.getSelectedItem().toString();
-                    String distance = log_distance.getText().toString();
-                    String metric = log_metric.getSelectedItem().toString();
-                    String minutes = log_time_minutes.getText().toString();
-                    String seconds = log_time_seconds.getText().toString();
-                    int feel = log_feel.getProgress() + 1;
-                    String description = log_location.getText().toString();
-
-                    boolean formError = false;
-
-                    if (distance.equals("") && seconds.equals("") && minutes.equals("") ||
-                            seconds.equals("") && minutes.equals("") && distance.equals("")) {
-                        log_error_message.setText("Either Distance or Time Must Be Entered");
-                        log_date_button.requestFocus();
-                        formError = true;
-                    }
-
-                    Pattern distance_pattern = Pattern.compile(DISTANCE_REGEX);
-                    Matcher distance_matcher = distance_pattern.matcher(distance);
-
-                    if (!distance_matcher.matches()) {
-                        log_error_message.setText("Invalid Distance Entered");
-                        log_distance.requestFocus();
-                        formError = true;
-                    }
-
-                    Pattern minute_pattern = Pattern.compile(MINUTE_REGEX);
-                    Matcher minute_matcher = minute_pattern.matcher(minutes);
-
-                    if (!minute_matcher.matches()) {
-                        log_error_message.setText("Invalid Minutes Entered");
-                        log_time_minutes.requestFocus();
-                        formError = true;
-                    }
-
-                    Pattern second_pattern = Pattern.compile(SECOND_REGEX);
-                    Matcher second_matcher = second_pattern.matcher(seconds);
-
-                    if (!second_matcher.matches() || Integer.parseInt(seconds) > 59) {
-                        log_error_message.setText("Invalid Seconds Entered");
-                        log_time_seconds.requestFocus();
-                        formError = true;
-                    }
-
-                    Calendar today = GregorianCalendar.getInstance(TimeZone.getTimeZone("EST"));
-                    Calendar calendar2016 = GregorianCalendar.getInstance();
-                    calendar2016.set(2016,0,1);
-
-                    if (calendar.compareTo(today) > 0) {
-                        log_error_message.setText("Invalid Date. The Date Selected Is In The Future");
-                        log_date_button.requestFocus();
-                        formError = true;
-                    } else if (calendar.compareTo(calendar2016) < 0) {
-                        log_error_message.setText("Invalid Date. The Date Must Be After Jan 1, 2016");
-                        log_date_button.requestFocus();
-                        formError = true;
-                    }
-
-                    // First Do Validation for the form inputs
-                    if (name.length() == 0) {
-                        log_error_message.setText("No Log Name Entered");
-                        log_run_name.requestFocus();
-                        formError = true;
-                    }
+                    name = log_run_name.getText().toString();
+                    location = log_location.getText().toString();
+                    type = log_type.getSelectedItem().toString();
+                    distance = log_distance.getText().toString();
+                    metric = log_metric.getSelectedItem().toString();
+                    minutes = log_time_minutes.getText().toString();
+                    seconds = log_time_seconds.getText().toString();
+                    feel = log_feel.getProgress() + 1;
+                    description = log_location.getText().toString();
 
                     // Second try to submit the log
-                    if (!formError) {
-
+                    if (!validateForms()) {
+                        log_error_message.setText("no error");
                     }
                 }
             });
@@ -255,4 +199,72 @@ public class LogDialogFragment extends DialogFragment {
         }
 
     };
+
+    private boolean validateForms() {
+
+        // First Do Validation for the form inputs
+        if (name.length() == 0) {
+            log_error_message.setText(R.string.no_log_name);
+            log_run_name.requestFocus();
+            return true;
+        }
+
+        Calendar today = GregorianCalendar.getInstance(TimeZone.getTimeZone("EST"));
+        Calendar calendar2016 = GregorianCalendar.getInstance();
+        calendar2016.set(2016,0,1);
+
+        if (calendar.compareTo(today) > 0) {
+            log_error_message.setText(R.string.invalid_date_future);
+            log_date_button.requestFocus();
+            return true;
+        } else if (calendar.compareTo(calendar2016) < 0) {
+            log_error_message.setText(R.string.invalid_date_past);
+            log_date_button.requestFocus();
+            return true;
+        }
+
+        if (distance.equals("") && seconds.equals("") && minutes.equals("")) {
+            log_error_message.setText(R.string.no_distance_or_time);
+            log_date_button.requestFocus();
+            return true;
+        }
+
+        Pattern distance_pattern = Pattern.compile(DISTANCE_REGEX);
+        Matcher distance_matcher = distance_pattern.matcher(distance);
+
+        Pattern minute_pattern = Pattern.compile(MINUTE_REGEX);
+        Matcher minute_matcher = minute_pattern.matcher(minutes);
+
+        Pattern second_pattern = Pattern.compile(SECOND_REGEX);
+        Matcher second_matcher = second_pattern.matcher(seconds);
+
+        if (!distance_matcher.matches()) {
+            if (distance.equals("") && minute_matcher.matches() && second_matcher.matches()) {
+                return false;
+            }
+            log_error_message.setText(R.string.invalid_distance);
+            log_distance.requestFocus();
+            return true;
+        }
+
+        if (minutes.equals("") && seconds.equals(""))
+            return false;
+
+        if (!minute_matcher.matches()) {
+            if (minutes.equals("") && (second_matcher.matches() && Integer.parseInt(seconds) <= 59)) {
+                return false;
+            }
+            log_error_message.setText(R.string.invalid_minutes);
+            log_time_minutes.requestFocus();
+            return true;
+        }
+
+        if (!second_matcher.matches() || Integer.parseInt(seconds) > 59) {
+            log_error_message.setText(R.string.invalid_seconds);
+            log_time_seconds.requestFocus();
+            return true;
+        }
+
+        return false;
+    }
 }
