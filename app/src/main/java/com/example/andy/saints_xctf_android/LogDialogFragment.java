686afc002b7860b101c00c63c59c4039a3a84456
Andrew Jarombek <ajarombek95@gmail.com>
2017-01-25 23:30:06 -0500

    V.0.0.31 - Log Form Validation Code Added

--- app/src/main/java/com/example/andy/saints_xctf_android/LogDialogFragment.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/LogDialogFragment.java
@@ -19,7 +19,10 @@ import android.widget.TextView;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Class for the Log DialogFragment which is used for creating new exercise logs
@@ -33,6 +36,9 @@ public class LogDialogFragment extends DialogFragment {
             MainActivity.DESCRIPT_MEDIOCRE, MainActivity.DESCRIPT_AVERAGE,
             MainActivity.DESCRIPT_FAIRLYGOOD, MainActivity.DESCRIPT_GOOD,
             MainActivity.DESCRIPT_GREAT, MainActivity.DESCRIPT_FANTASTIC};
+    private static final String DISTANCE_REGEX = "^[0-9]{0,3}(\\.[0-9]{1,2})?$";
+    private static final String MINUTE_REGEX = "^[0-9]{1,5}$";
+    private static final String SECOND_REGEX = "^[0-9]{1,2}$";
 
     // Views in the fragment
     private TextView log_feel_view;
@@ -106,7 +112,8 @@ public class LogDialogFragment extends DialogFragment {
         metricAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         log_metric.setAdapter(metricAdapter);
 
-        calendar = Calendar.getInstance(TimeZone.getTimeZone("EST"));
+        // Get the current date and set the log date text
+        calendar = GregorianCalendar.getInstance(TimeZone.getTimeZone("EST"));
         log_date.setText((calendar.get(Calendar.MONTH) + 1) + "-" +
                 calendar.get(Calendar.DAY_OF_MONTH) + "-" + calendar.get(Calendar.YEAR));
 
@@ -121,9 +128,11 @@ public class LogDialogFragment extends DialogFragment {
             }
         });
 
+        // As the user slides the seekbar, make changes
         log_feel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Change the text value for the logs feel
                 log_feel_view.setText(COLOR_DESCRIPTION[progress].toUpperCase());
             }
 
@@ -162,18 +171,66 @@ public class LogDialogFragment extends DialogFragment {
                     String name = log_run_name.getText().toString();
                     String location = log_location.getText().toString();
                     String type = log_type.getSelectedItem().toString();
-                    double distance = Double.parseDouble(log_distance.getText().toString());
+                    String distance = log_distance.getText().toString();
                     String metric = log_metric.getSelectedItem().toString();
-                    int minutes = Integer.parseInt(log_time_minutes.getText().toString());
-                    int seconds = Integer.parseInt(log_time_seconds.getText().toString());
+                    String minutes = log_time_minutes.getText().toString();
+                    String seconds = log_time_seconds.getText().toString();
                     int feel = log_feel.getProgress() + 1;
                     String description = log_location.getText().toString();
 
                     boolean formError = false;
 
+                    if (distance.equals("") && seconds.equals("") && minutes.equals("") ||
+                            seconds.equals("") && minutes.equals("") && distance.equals("")) {
+                        log_error_message.setText("Either Distance or Time Must Be Entered");
+                        log_date_button.requestFocus();
+                        formError = true;
+                    }
+
+                    Pattern distance_pattern = Pattern.compile(DISTANCE_REGEX);
+                    Matcher distance_matcher = distance_pattern.matcher(distance);
+
+                    if (!distance_matcher.matches()) {
+                        log_error_message.setText("Invalid Distance Entered");
+                        log_distance.requestFocus();
+                        formError = true;
+                    }
+
+                    Pattern minute_pattern = Pattern.compile(MINUTE_REGEX);
+                    Matcher minute_matcher = minute_pattern.matcher(minutes);
+
+                    if (!minute_matcher.matches()) {
+                        log_error_message.setText("Invalid Minutes Entered");
+                        log_time_minutes.requestFocus();
+                        formError = true;
+                    }
+
+                    Pattern second_pattern = Pattern.compile(SECOND_REGEX);
+                    Matcher second_matcher = second_pattern.matcher(seconds);
+
+                    if (!second_matcher.matches() || Integer.parseInt(seconds) > 59) {
+                        log_error_message.setText("Invalid Seconds Entered");
+                        log_time_seconds.requestFocus();
+                        formError = true;
+                    }
+
+                    Calendar today = GregorianCalendar.getInstance(TimeZone.getTimeZone("EST"));
+                    Calendar calendar2016 = GregorianCalendar.getInstance();
+                    calendar2016.set(2016,0,1);
+
+                    if (calendar.compareTo(today) > 0) {
+                        log_error_message.setText("Invalid Date. The Date Selected Is In The Future");
+                        log_date_button.requestFocus();
+                        formError = true;
+                    } else if (calendar.compareTo(calendar2016) < 0) {
+                        log_error_message.setText("Invalid Date. The Date Must Be After Jan 1, 2016");
+                        log_date_button.requestFocus();
+                        formError = true;
+                    }
+
                     // First Do Validation for the form inputs
                     if (name.length() == 0) {
-                        log_error_message.setText(R.string.no_password);
+                        log_error_message.setText("No Log Name Entered");
                         log_run_name.requestFocus();
                         formError = true;
                     }
@@ -194,7 +251,7 @@ public class LogDialogFragment extends DialogFragment {
         @Override
         public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
             log_date.setText((monthOfYear + 1) + "-" + dayOfMonth + "-" + year);
-            calendar.set(year + 1900, monthOfYear, dayOfMonth);
+            calendar.set(year, monthOfYear, dayOfMonth);
         }
 
     };
