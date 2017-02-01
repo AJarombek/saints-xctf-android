3c682b65ae73f6f936c4eb09bf3455592824a51b
Andrew Jarombek <ajarombek95@gmail.com>
2017-01-31 19:04:09 -0500

    V.0.0.49 - Edit Profile Backend Work

--- app/src/main/java/com/example/andy/saints_xctf_android/EditProfileFragment.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/EditProfileFragment.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
@@ -22,14 +23,20 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.andy.api_model.APIClient;
 import com.example.andy.api_model.JSONConverter;
 import com.example.andy.api_model.User;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -40,6 +47,9 @@ public class EditProfileFragment extends Fragment {
     private static final String TAG = EditProfileFragment.class.getName();
     public static final String PREFS_NAME = "SaintsxctfUserPrefs";
     private static final int RESULT_LOAD_IMG = 0;
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    private static final String NAME_REGEX = "^[a-zA-Z\\-']+$";
+    private static final String YEAR_REGEX = "^[0-9]+$";
 
     private View v;
     private EditText edit_profile_first;
@@ -49,8 +59,10 @@ public class EditProfileFragment extends Fragment {
     private EditText edit_profile_location;
     private EditText edit_profile_favorite_event;
     private EditText edit_profile_description;
+
     private ImageView edit_profile_picture;
     private Button edit_profile_picture_button;
+    private Bitmap profile_picture_bitmap;
 
     private Button edit_profile_mensxc;
     private Button edit_profile_wmensxc;
@@ -59,8 +71,11 @@ public class EditProfileFragment extends Fragment {
     private Button edit_profile_alumni;
     private boolean mensxc, wmensxc, menstf, wmenstf, alumni;
 
+    private TextView edit_profile_error_message;
     private Button edit_profile_cancel;
     private Button edit_profile_submit;
+    private User user;
+    private String username;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -71,7 +86,7 @@ public class EditProfileFragment extends Fragment {
                 PREFS_NAME, Context.MODE_PRIVATE);
 
         String userJSON = prefs.getString("user", "");
-        User user = null;
+        user = null;
 
         try {
             user = JSONConverter.toUser(userJSON);
@@ -97,11 +112,14 @@ public class EditProfileFragment extends Fragment {
         edit_profile_wmenstf = (Button) v.findViewById(R.id.edit_profile_wmenstf);
         edit_profile_alumni = (Button) v.findViewById(R.id.edit_profile_alumni);
 
+        edit_profile_error_message = (TextView) v.findViewById(R.id.edit_profile_error_message);
         edit_profile_cancel = (Button) v.findViewById(R.id.edit_profile_cancel);
         edit_profile_submit = (Button) v.findViewById(R.id.edit_profile_submit);
 
         edit_profile_first.setText(user.getFirst());
         edit_profile_last.setText(user.getLast());
+
+        username = user.getUsername();
 
         String email = user.getEmail();
         if (email != null)
@@ -129,8 +147,8 @@ public class EditProfileFragment extends Fragment {
             base64encoding = base64encoding.replace("data:image/jpeg;base64,", "");
 
             byte[] decodedString = Base64.decode(base64encoding, Base64.DEFAULT);
-            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-            edit_profile_picture.setImageBitmap(decodedByte);
+            profile_picture_bitmap = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            edit_profile_picture.setImageBitmap(profile_picture_bitmap);
             edit_profile_picture.setBackground(null);
         }
 
@@ -477,7 +495,104 @@ public class EditProfileFragment extends Fragment {
             }
         });
 
+        edit_profile_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().popBackStackImmediate();
+            }
+        });
+
+        edit_profile_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validateFields()) {
+                    updateUser();
+
+                    String userString = "";
+                    try {
+                        userString = JSONConverter.fromUser(user);
+                    } catch (Throwable throwable) {
+                        Log.e(TAG, "User object JSON conversion failed.");
+                        Log.e(TAG, throwable.getMessage());
+                        edit_profile_error_message.setText(R.string.internal_error);
+                        return;
+                    }
+
+                    UpdateUserTask updateUserTask = new UpdateUserTask();
+                    updateUserTask.execute(username, userString);
+                }
+            }
+        });
+
         return v;
+    }
+
+    private boolean validateFields() {
+        String first = edit_profile_first.getText().toString();
+        String last = edit_profile_last.getText().toString();
+        String email = edit_profile_email.getText().toString();
+        String year = edit_profile_class_year.getText().toString();
+
+        Pattern email_pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher email_matcher = email_pattern.matcher(email);
+        Pattern name_pattern = Pattern.compile(NAME_REGEX);
+        Matcher first_matcher = name_pattern.matcher(first);
+        Matcher last_matcher = name_pattern.matcher(last);
+        Pattern year_pattern = Pattern.compile(YEAR_REGEX);
+        Matcher year_matcher = year_pattern.matcher(year);
+
+        if (!first_matcher.matches()) {
+            edit_profile_error_message.setText(R.string.invalid_first_name);
+            edit_profile_first.requestFocus();
+            return false;
+        }
+        if (!last_matcher.matches()) {
+            edit_profile_error_message.setText(R.string.invalid_last_name);
+            edit_profile_last.requestFocus();
+            return false;
+        }
+        if (!email_matcher.matches()) {
+            edit_profile_error_message.setText(R.string.invalid_email);
+            edit_profile_email.requestFocus();
+            return false;
+        }
+        if (!year_matcher.matches()) {
+            edit_profile_error_message.setText(R.string.invalid_class_year);
+            edit_profile_class_year.requestFocus();
+            return false;
+        }
+        return true;
+    }
+
+    private void updateUser() {
+        user.setFirst(edit_profile_first.getText().toString());
+        user.setLast(edit_profile_last.getText().toString());
+        user.setEmail(edit_profile_email.getText().toString());
+        user.setClass_year(Integer.parseInt(edit_profile_class_year.getText().toString()));
+        user.setLocation(edit_profile_location.getText().toString());
+        user.setFavorite_event(edit_profile_favorite_event.getText().toString());
+        user.setDescription(edit_profile_description.getText().toString());
+
+        // convert from bitmap to base 64 encoded string
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        profile_picture_bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream.toByteArray();
+
+        String encoded = Base64.encodeToString(byteArray, Base64.DEFAULT);
+        user.setProfilepic(encoded);
+
+        Map<String,String> groups = new HashMap<>();
+        if (mensxc)
+            groups.put("mensxc", "Men's Cross Country");
+        if (wmensxc)
+            groups.put("wmensxc", "Women's Cross Country");
+        if (menstf)
+            groups.put("menstf", "Men's Track & Field");
+        if (wmenstf)
+            groups.put("wmenstf", "Women's Track & Field");
+        if (alumni)
+            groups.put("alumni", "Alumni");
+        user.setGroups(groups);
     }
 
     @Override
@@ -492,8 +607,8 @@ public class EditProfileFragment extends Fragment {
                 try {
                     InputStream is = getActivity().getContentResolver().openInputStream(selectedImageUri);
                     if (is != null) {
-                        Bitmap pictureBitmap = BitmapFactory.decodeStream(is);
-                        edit_profile_picture.setImageBitmap(pictureBitmap);
+                        profile_picture_bitmap = BitmapFactory.decodeStream(is);
+                        edit_profile_picture.setImageBitmap(profile_picture_bitmap);
                         edit_profile_picture.setBackground(null);
                     }
                 } catch (FileNotFoundException e) {
@@ -501,23 +616,6 @@ public class EditProfileFragment extends Fragment {
                 }
             }
         }
-    }
-
-    public String getPath(Uri uri, Activity activity) {
-        Cursor cursor = null;
-        try {
-            String[] projection = {MediaStore.MediaColumns.DATA};
-            cursor = activity.getContentResolver().query(uri, projection, null, null, null);
-            if (cursor.moveToFirst()) {
-                int column_index = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-                return cursor.getString(column_index);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            cursor.close();
-        }
-        return "";
     }
 
     private void clickGroup(String group) {
@@ -537,6 +635,57 @@ public class EditProfileFragment extends Fragment {
             case "alumni":
                 edit_profile_alumni.performClick();
                 break;
+        }
+    }
+
+    class UpdateUserTask extends AsyncTask<String, Void, Object> {
+
+        @Override
+        protected Object doInBackground(String... strings) {
+            User user = null;
+            try {
+                user = APIClient.userPutRequest(strings[0], strings[1]);
+
+                if (user == null)
+                    return "no_internet";
+
+            } catch (Exception e) {
+                Log.e(TAG, "User object JSON conversion failed.");
+                Log.e(TAG, e.getMessage());
+                return "no_internet";
+            }
+
+            return user;
+        }
+
+        @Override
+        protected void onPostExecute(Object response) {
+            super.onPostExecute(response);
+
+            if (response != null) {
+                if (response.equals("no_internet")) {
+                    ((MainActivity) getActivity()).noInternet();
+                } else if (response instanceof User) {
+                    user = (User) response;
+
+                    String userString = "";
+                    try {
+                        userString = JSONConverter.fromUser(user);
+                    } catch (Throwable throwable) {
+                        Log.e(TAG, "User object JSON conversion failed.");
+                        Log.e(TAG, throwable.getMessage());
+                    }
+
+                    SharedPreferences sp = getActivity().getSharedPreferences(PREFS_NAME, 0);
+                    SharedPreferences.Editor editor = sp.edit();
+                    editor.putString("user", userString);
+                    editor.putString("first", user.getFirst());
+                    editor.putString("last", user.getLast());
+                    editor.apply();
+
+                    getFragmentManager().popBackStackImmediate();
+                }
+            }
         }
     }
 
