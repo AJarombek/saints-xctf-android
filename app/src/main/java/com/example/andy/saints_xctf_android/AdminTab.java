1239fa863edfe15853f065dd11e909b5383b140e
Andrew Jarombek <ajarombek95@gmail.com>
2017-07-22 17:32:47 -0400

    V.0.0.93 - Admin UI Done and Dropdown Populates

--- app/src/main/java/com/example/andy/saints_xctf_android/AdminTab.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/AdminTab.java
@@ -5,6 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.example.andy.api_model.Group;
+import com.example.andy.api_model.GroupMember;
+import com.example.andy.api_model.JSONConverter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,18 +24,45 @@ import android.view.ViewGroup;
 public class AdminTab extends Fragment {
 
     private View v;
+    private Group group;
+    private Spinner group_users_dropdown;
+    private ArrayList<String> member_names;
+    private HashMap<String, String> member_map;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.monthly_view_tab, container, false);
+        View view = inflater.inflate(R.layout.admin_tab, container, false);
         v = view;
 
-        // Bundle bundle = getArguments();
-        // username = bundle.getString("username", "");
+        group_users_dropdown = (Spinner) v.findViewById(R.id.group_users_dropdown);
 
-        // monthly_view = (WebView) v.findViewById(R.id.monthly_web_view);
+        Bundle bundle = getArguments();
+        String groupString = bundle.getString("group", "");
+
+        try {
+            group = JSONConverter.toGroup(groupString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        member_names = new ArrayList<>();
+        member_map = new HashMap<>();
+        ArrayList<GroupMember> groupMembers = group.getMembers();
+
+        for (GroupMember member : groupMembers) {
+            String name = member.getFirst() + " " + member.getLast();
+            member_names.add(name);
+            member_map.put(name, member.getUsername());
+        }
+
+        // Populate the spinner with all the group members names
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<>(getContext(),
+                android.R.layout.simple_spinner_dropdown_item, member_names);
+
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        group_users_dropdown.setAdapter(typeAdapter);
 
         return view;
     }
