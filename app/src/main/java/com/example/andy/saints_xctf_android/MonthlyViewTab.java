54a747e14c2f89a14bd87d38107a6172be236c7a
Andrew Jarombek <ajarombek95@gmail.com>
2017-07-27 19:35:40 -0400

    V.0.1.1 - Restructure Monthly Calendar Away from XML

--- app/src/main/java/com/example/andy/saints_xctf_android/MonthlyViewTab.java
+++ app/src/main/java/com/example/andy/saints_xctf_android/MonthlyViewTab.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,15 @@ import android.view.ViewGroup;
 public class MonthlyViewTab extends Fragment {
 
     private View v;
+    private ArrayList<String> calIds;
+    private ArrayList<String> caldayIds;
+    private ArrayList<String> calmilesIds;
+
+    private ArrayList<GridLayout> calendar_grids;
+    private ArrayList<TextView> calendar_days;
+    private ArrayList<TextView> calendar_miles;
+
+    private ArrayList<TextView> calendar_total_miles;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,6 +34,17 @@ public class MonthlyViewTab extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.monthly_view_tab, container, false);
         v = view;
+
+        calIds = new ArrayList<>();
+
+        // Generate lists of all the calendar index ids
+        for (int i = 0; i < 48; i++) {
+            int row = i / 8;
+            int col = i % 8;
+            calIds.add("cal_" + row + "_" + col);
+            caldayIds.add("calday_" + row + "_" + col);
+            calmilesIds.add("calmiles_" + row + "_" + col);
+        }
 
         // Bundle bundle = getArguments();
         // username = bundle.getString("username", "");
